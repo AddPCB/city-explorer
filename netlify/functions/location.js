8c6ba26fc83bdfd9cc13b3c4f049c8384362e61f
Peter Brightmore <P.C.Brightmore@gmail.com>
2023-03-06 16:43:52 +0000

    new version of netlify function

--- netlify/functions/location.js
+++ netlify/functions/location.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 
 exports.handler = async (event) => {
-  const API_KEY = process.env.REACT_APP_CITY_KEY;
-  const { searchQuery } = JSON.parse(event.body);
+  const API_KEY = process.env.CITY_KEY;
+  const { searchQuery } = event.queryStringParameters;
 
   const API = `https://eu1.locationiq.com/v1/search.php?key=${API_KEY}&q=${searchQuery}&format=json`;
   const response = await axios.get(API);
