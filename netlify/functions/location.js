d6c2e9a0ad011c98decf82389a0a2649b19af4a6
Peter Brightmore <P.C.Brightmore@gmail.com>
2023-03-06 16:44:16 +0000

    .

--- netlify/functions/location.js
+++ netlify/functions/location.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 exports.handler = async (event) => {
-  const API_KEY = process.env.CITY_KEY;
+  const API_KEY = process.env.REACT_APP_CITY_KEY;
   const { searchQuery } = event.queryStringParameters;
 
   const API = `https://eu1.locationiq.com/v1/search.php?key=${API_KEY}&q=${searchQuery}&format=json`;
