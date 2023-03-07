7cf79163e826dcd3c94d9651b432104d6022e56b
Peter Brightmore <P.C.Brightmore@gmail.com>
2023-03-07 14:23:30 +0000

    .

--- functions/location.js
+++ functions/location.js
@@ -4,13 +4,13 @@ exports.handler = async (event) => {
   const API_KEY = process.env.REACT_APP_CITY_KEY;
   const { searchQuery } = event.queryStringParameters;
 
-  const API = `https://eu1.locationiq.com/v1/search.php?key=${API_KEY}&q=${searchQuery}&format=json`;
+  const API = `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${searchQuery}&format=json`;
   const response = await axios.get(API);
 
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': 'https://explore-new-cities.netlify.app/netlify/functions/location',
+      "Access-Control-Allow-Origin": "https://explore-new-cities.netlify.app, http://localhost:8888, http://localhost:3000"
     },
     body: JSON.stringify(response.data[0]),
   };
